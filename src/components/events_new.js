62eead865d46d1a7d94e24ffeb84d37dfd0cebcf
yuma11 <11yuma20@gmail.com>
2020-09-13 12:01:44 +0900

    implement event new component

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -3,18 +3,35 @@ import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
 
-// import { postEvent } from '../actions'
+import { postEvent } from '../actions'
 
 class EventsNew extends Component {
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
   renderField(field) {
     const { input, label, type, meta: { touched, error } } = field
 
-    return (<div></div>)
+    return (
+      <div>
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    )
+  }
+
+  async onSubmit(values) {
+    await this.props.postEvent(values)
+    this.props.history.push('/')
   }
 
   render() {
+    const { handleSubmit } = this.props
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label="Title" name="title" type="text" component={this.renderField}/></div>
         <div><Field label="Body" name="body" type="text" component={this.renderField}/></div>
 
@@ -30,10 +47,14 @@ class EventsNew extends Component {
 
 const validate = values => {
   const errors = {}
+
+  if (!values.title) errors.title = "Enter a title please."
+  if (!values.body) errors.body = "Enter a body please."
+
   return errors
 }
-// const mapDispatchToProps = ({ postEvent })
+const mapDispatchToProps = ({ postEvent })
 
-export default connect(null, null)(
+export default connect(null, mapDispatchToProps)(
   reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
 )
